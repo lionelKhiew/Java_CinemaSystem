90227d3f7d23bd797fb5cfb7634b33b55d8fdff3
JianChunMa <141934943+JianChunMa@users.noreply.github.com>
2023-08-20 20:10:22 +0800

    Add files via upload

---
+++ LoginandSignUp.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ass;
+
+/**
+ *
+ * @author MA JIAN CHUN
+ */
+public class LoginandSignUp {
+    public static void main(String[] args){
+        Login loginframe=new Login();
+        loginframe.setVisible(true);
+        loginframe.setResizable(false);
+        loginframe.pack();
+        loginframe.setLocationRelativeTo(null);
+        
+        
+    }
+}
